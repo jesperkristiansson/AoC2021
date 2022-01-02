0ba160536318ae215adcb67816f60edc9f5a7d5f
jesperkristiansson <jeppe.00@live.se>
2022-01-02 11:46:53 +0100

    saving

--- src/day15/Alt.java
+++ src/day15/Alt.java
@@ -1,8 +1,12 @@
 package day15;
 
+import java.awt.Point;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import helpers.Helpers;
 
@@ -16,59 +20,68 @@ public class Alt {
 	static int part2(String[] input) {
 		int height = input.length;
 		int width = input[0].length();
-		Set<Position> unvisited = new HashSet<Position>();
-		Position start = new Position(0, 0, 0, 0);
-		unvisited.add(start);
-		Position end = new Position(width * 5 - 1, height * 5 - 1, 0, 0);
+		Map<Point, Node> unvisited = new HashMap<Point, Node>();
+		Point end = new Point(width * 5 - 1, height * 5 - 1);
 		for (int y = 0; y < height; y++) {
 			for (int x = 0; x < width; x++) {
 				int baseCost = Character.getNumericValue(input[y].charAt(x));
 				for (int r = 0; r < 5; r++) {
 					for (int c = 0; c < 5; c++) {
-						int newX = x + width*c;
-						int newY = y + height*r;
+						int newX = x + width * c;
+						int newY = y + height * r;
 						int newCost = newCost(baseCost, r, c);
-						unvisited.add(new Position(newX, newY, newCost, Integer.MAX_VALUE));
+						unvisited.put(new Point(newX, newY), new Node(newCost, Integer.MAX_VALUE));
 					}
 				}
 			}
 		}
+		unvisited.replace(new Point(0, 0), new Node(0, 0));
 		return costToEnd(unvisited, end);
 	}
-	
+
 	static int newCost(int baseCost, int r, int c) {
 		int newCost = (baseCost + r + c) % 9;
-		if(newCost == 0) {
+		if (newCost == 0) {
 			newCost = 9;
 		}
 		return newCost;
 	}
 
-	static int part1(String[] input) {
-		int height = input.length;
-		int width = input[0].length();
-		Set<Position> unvisited = new HashSet<Position>();
-		Position start = new Position(0, 0, 0, 0);
-		unvisited.add(start);
-		Position end = new Position(width - 1, height - 1, 0, 0);
-		for (int y = 0; y < height; y++) {
-			for (int x = 0; x < width; x++) {
-				int cost = Character.getNumericValue(input[y].charAt(x));
-				unvisited.add(new Position(x, y, cost, Integer.MAX_VALUE));
-			}
-		}
-		return costToEnd(unvisited, end);
-	}
+	/*
+	 * static int part1(String[] input) { int height = input.length; int width =
+	 * input[0].length(); Set<Position> unvisited = new HashSet<Position>();
+	 * Position start = new Position(0, 0, 0, 0); unvisited.add(start); Position end
+	 * = new Position(width - 1, height - 1, 0, 0); for (int y = 0; y < height; y++)
+	 * { for (int x = 0; x < width; x++) { int cost =
+	 * Character.getNumericValue(input[y].charAt(x)); unvisited.add(new Position(x,
+	 * y, cost, Integer.MAX_VALUE)); } } return costToEnd(unvisited, end); }
+	 */
 
-	static int costToEnd(Set<Position> unvisited, Position end) {
-		while (!unvisited.isEmpty()) {
-			Position current = cheapest(unvisited);
-			System.out.println(current);
-			if (current.equals(end)) {
-				return current.totalCost();
+	static int costToEnd(Map<Point, Node> unvisited, Point end) {
+		Map<Point, Collection<Node>> neighbors = new HashMap<Point, Collection<Node>>();
+		for (Map.Entry<Point, Node> entry : unvisited.entrySet()) {
+			int x = entry.getKey().x;
+			int y = entry.getKey().y;
+			List<Node> neighs = new ArrayList<Node>();
+			neighs.add(unvisited.get(new Point(x+1, y)));
+			neighs.add(unvisited.get(new Point(x, y+1)));
+			neighs.add(unvisited.get(new Point(x-1, y)));
+			neighs.add(unvisited.get(new Point(x, y-1)));
+			List<Node> realNeighs = new ArrayList<Node>();
+			for(Node node : neighs) {
+				if(node != null) {
+					realNeighs.add(node);
+				}
 			}
-			for (Position pos : current.neighbors(unvisited)) {
-				int newCost = current.totalCost() + pos.cost();
+			neighbors.put(entry.getKey(), realNeighs);
+		}
+		while (!unvisited.isEmpty()) {
+			Point current = cheapest(unvisited);
+			if (current.equals(end)) {
+				return unvisited.get(current).totalCost();
+			}
+			for (Node pos : neighbors.get(current)) {
+				int newCost = unvisited.get(current).totalCost() + pos.cost();
 				if (newCost < pos.totalCost()) {
 					pos.setTotalCost(newCost);
 				}
@@ -78,13 +91,14 @@ public class Alt {
 		return -1;
 	}
 
-	static Position cheapest(Iterable<Position> positions) {
+	static Point cheapest(Map<Point, Node> map) {
 		int lowestCost = Integer.MAX_VALUE;
-		Position ret = null;
-		for (Position pos : positions) {
-			if (pos.totalCost() < lowestCost) {
-				lowestCost = pos.totalCost();
-				ret = pos;
+		Point ret = null;
+		for (Map.Entry<Point, Node> entry : map.entrySet()) {
+			Node node = entry.getValue();
+			if (node.totalCost() < lowestCost) {
+				lowestCost = node.totalCost();
+				ret = entry.getKey();
 			}
 		}
 		return ret;
--- src/day15/Node.java
+++ src/day15/Node.java
@@ -1,45 +1,33 @@
 package day15;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+public class Node implements Comparable<Node>{
+	private int cost;
+	private int totalCost;
+	
+	public Node(int cost, int totalCost){
+		this.cost = cost;
+		this.totalCost = totalCost;
+	}
+	
+	public int cost() {
+		return cost;
+	}
+	
+	public int totalCost() {
+		return totalCost;
+	}
+	
+	public void setTotalCost(int newCost) {
+		totalCost = newCost;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("cost: %d totalCost: %d", cost, totalCost);
+	}
 
-public class Node {
-    
-    public String name;
-    
-    private List<Node> shortestPath = new LinkedList<>();
-    
-    private Integer distance = Integer.MAX_VALUE;
-    
-    Map<Node, Integer> adjacentNodes = new HashMap<>();
-
-    public void addDestination(Node destination, int distance) {
-        adjacentNodes.put(destination, distance);
-    }
- 
-    public Node(String name) {
-        this.name = name;
-    }
-    
-    public void setDistance(int dist) {
-    	distance = dist;
-    }
-    
-    public int getDistance() {
-    	return distance;
-    }
-    
-    public Map<Node, Integer> getAdjacentNodes(){
-    	return adjacentNodes;
-    }
-    
-    public List<Node> getShortestPath(){
-    	return shortestPath;
-    }
-    
-    public void setShortestPath(List<Node> path) {
-    	shortestPath = path;
-    }
+	@Override
+	public int compareTo(Node o) {
+		return totalCost - o.totalCost;
+	}
 }
